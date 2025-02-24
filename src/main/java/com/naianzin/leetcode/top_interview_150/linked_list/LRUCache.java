e68e3529cc7d4326f2397bc67cd01cf7e7bde028
alex-canada-waterloo <alex.canada.waterloo@gmail.com>
2025-02-24 09:55:21 -0500

    + 1 linked list task

--- src/main/java/com/naianzin/leetcode/top_interview_150/linked_list/LRUCache.java
+++ src/main/java/com/naianzin/leetcode/top_interview_150/linked_list/LRUCache.java
@@ -1,7 +1,6 @@
 package com.naianzin.leetcode.top_interview_150.linked_list;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 
 // https://leetcode.com/problems/lru-cache/description/?envType=study-plan-v2&envId=top-interview-150
 public class LRUCache {
@@ -13,10 +12,10 @@ public class LRUCache {
         public Entry next;
     }
 
-    private HashMap<Integer, Entry> map;
+    private final HashMap<Integer, Entry> map;
+    private final int capacity;
     private Entry head;
     private Entry tail;
-    private int capacity;
 
 
     public LRUCache(int capacity) {
@@ -25,51 +24,91 @@ public class LRUCache {
     }
 
     public int get(int key) {
-        if (map.containsKey(key)) {
-            var entry = map.get(key);
-
-            if (head == entry) {
-                return entry.val;
-            } else if (tail == entry) {
-                tail = entry.prev;
-                entry.next = head;
-                head.prev = entry;
-                head = entry;
-            } else {
-                entry.prev.next = entry.next;
-                entry.next.prev = entry.prev;
-                entry.next = head;
-                head.prev = entry;
-                head = entry;
-            }
-
-            return entry.val;
-
-        } else {
+        var entry = map.get(key);
+        if (entry == null) {
             return -1;
         }
+        updateUsage(entry);
+        return entry.val;
     }
 
     public void put(int key, int value) {
-        var entry = new Entry();
-        entry.key = key;
+       var entry = map.get(key);
+       if (entry == null) {
+           insertNewKey(key, value);
+       } else {
+           updateExistingEntry(entry, value);
+       }
+    }
+
+    private void updateExistingEntry(Entry entry, int value) {
         entry.val = value;
-        if (head == null) {
-            head = entry;
+        updateUsage(entry);
+    }
+
+    private void updateUsage(Entry entry) {
+        if (entry == head) {
+            return;
+        }
+
+        // remove node
+        if (entry == tail) {
+            tail.prev.next = null;
+            tail = tail.prev;
+            entry.prev = null;
+            entry.next = null;
         } else {
+            var leftEntry = entry.prev;
+            var rightEntry = entry.next;
+            entry.prev = null;
+            entry.next = null;
+
+            if (leftEntry != null) {
+                leftEntry.next = rightEntry;
+            }
+
+            if (rightEntry != null) {
+                rightEntry.prev = leftEntry;
+            }
+        }
+
+
+        // Insert node at head
+        if (head != null) {
             entry.next = head;
             head.prev = entry;
             head = entry;
         }
-        if (tail == null) {
-            tail = entry;
-        }
+    }
+
+    private void insertNewKey(int key, int value) {
+
+        ensureCapacity();
+
+        var entry = new Entry();
+        entry.key = key;
+        entry.val = value;
         map.put(key, entry);
 
-        if (map.size() == capacity) {
+        // new key to head
+        if (head != null) {
+            entry.next = head;
+            head.prev = entry;
+        } else {
+            tail = entry;
+        }
+        head = entry;
+    }
+
+    private void ensureCapacity() {
+        if (map.size() == this.capacity) {
             map.remove(tail.key);
-            if (tail.prev != null) {
+            if (map.isEmpty()) {
+                head = null;
+                tail = null;
+            } else {
                 tail = tail.prev;
+                tail.next = null;
             }
         }
     }
@@ -77,11 +116,25 @@ public class LRUCache {
     public static void main(String[] args) {
 
         var cache = new LRUCache(2);
-        cache.put(2,1);
         cache.put(1,1);
-        cache.put(2,3);
-        cache.put(4,1);
+        cache.put(2,2);
+        System.out.println(cache.get(1) + " Expected: 1");
+        cache.put(3,3);
+        System.out.println(cache.get(2) + " Expected: -1");
+        cache.put(4,4);
         System.out.println(cache.get(1) + " Expected: -1");
-        System.out.println(cache.get(2) + " Expected: 3");
+        System.out.println(cache.get(3) + " Expected: 3");
+        System.out.println(cache.get(4) + " Expected: 4");
+
+        var cache2 = new LRUCache(2);
+        cache2.put(2,1);
+        cache2.put(1,1);
+        cache2.put(2,3);
+        cache2.put(4,1);
+        System.out.println(cache2.get(1) + " Expected: -1");
+        System.out.println(cache2.get(2) + " Expected: 3");
+
+
+
     }
 }
